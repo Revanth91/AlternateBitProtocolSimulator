a3fb9cc329342aa4f93dc6a69cc5bc196937289c
Revanth91 <revanthsridhar@gmail.com>
2019-08-02 13:28:13 -0400

    applied <80 characters per line coding conventions

--- include/atomics/receiverCadmium.hpp
+++ include/atomics/receiverCadmium.hpp
@@ -39,74 +39,80 @@ template<typename TIME>
 class Receiver {
     using defs = receiver_defs; // putting definitions in context
     public:
-    //Parameters to be overwriten when instantiating the atomic model
-    TIME PREPARATION_TIME;
+        //Parameters to be overwriten when instantiating the atomic model
+        TIME PREPARATION_TIME;
     
-    // default constructor
-    Receiver() noexcept {
-        PREPARATION_TIME = TIME("00:00:10");
-        state.acknowledgement_number = 0;
-        state.sending = false;
-    }
-            
-    // state definition
-    struct state_type {
-        int acknowledgement_number;
-        bool sending;
-    }; 
-    state_type state;
-    
-    // ports definition
-    using input_ports=std::tuple<typename defs::in>;
-    using output_ports=std::tuple<typename defs::out>;
-
-    // internal transition
-    void internal_transition() {
-        state.sending = false; 
-    }
-
-    // external transition
-    void external_transition(TIME e, typename make_message_bags<input_ports>::type mbs) { 
-        if (get_messages <typename defs::in> (mbs).size() > 1) {
-            assert(false && "one message per time uniti");    
-        } 
-        for (const auto &x : get_messages<typename defs::in> (mbs)) {
-            state.acknowledgement_number = static_cast <int> (x.value);
-            state.sending = true;
-        }            
-    }
-
-    // confluence transition
-    void confluence_transition(TIME e, typename make_message_bags<input_ports>::type mbs) {
-        internal_transition();
-        external_transition(TIME(), std::move(mbs));
-    }
-
-    // output function
-    typename make_message_bags<output_ports>::type output() const {
-        typename make_message_bags<output_ports>::type bags;
-        message_t out;              
-        out.value = state.acknowledgement_number % 10;
-        get_messages <typename defs::out> (bags).push_back(out);       
-        return bags;
-    }
-
-    // time_advance function
-    TIME time_advance() const {  
-        TIME next_internal;
-        if (state.sending) {
-            next_internal = PREPARATION_TIME;
+        // default constructor
+        Receiver() noexcept {
+            PREPARATION_TIME = TIME("00:00:10");
+            state.acknowledgement_number = 0;
+            state.sending = false;
         }
-        else {
-            next_internal = std::numeric_limits<TIME>::infinity();
-        }    
-        return next_internal;
-    }
+            
+        // state definition
+        struct state_type {
+            int acknowledgement_number;
+            bool sending;
+        }; 
+        state_type state;
+        
+        // ports definition
+        using input_ports = std::tuple<typename defs::in>;
+        using output_ports = std::tuple<typename defs::out>;
 
-    friend std::ostringstream& operator<<(std::ostringstream& os, const typename Receiver<TIME>::state_type& i) {
-        os << "acknowledgement_number: " << i.acknowledgement_number; 
-        return os;
-    }
+        // internal transition
+        void internal_transition() {
+            state.sending = false; 
+        }
+
+        // external transition
+        void external_transition(
+                TIME e, 
+                typename make_message_bags<input_ports>::type mbs) { 
+            if (get_messages<typename defs::in>(mbs).size() > 1) {
+                assert(false && "one message per time uniti");    
+            } 
+            for (const auto &x : get_messages<typename defs::in>(mbs)) {
+                state.acknowledgement_number = static_cast<int>(x.value);
+                state.sending = true;
+            }            
+        }
+
+        // confluence transition
+        void confluence_transition(
+                TIME e, 
+                typename make_message_bags<input_ports>::type mbs) {
+            internal_transition();
+            external_transition(TIME(), std::move(mbs));
+        }
+
+        // output function
+        typename make_message_bags<output_ports>::type output() const {
+            typename make_message_bags<output_ports>::type bags;
+            message_t out;              
+            out.value = state.acknowledgement_number % 10;
+            get_messages<typename defs::out>(bags).push_back(out);       
+            return bags;
+        }
+
+        // time_advance function
+        TIME time_advance() const {  
+            TIME next_internal;
+            if (state.sending) {
+                next_internal = PREPARATION_TIME;
+            }
+            else {
+                next_internal = std::numeric_limits<TIME>::infinity();
+            }    
+            return next_internal;
+        }
+
+        friend std::ostringstream& operator<<(
+                std::ostringstream& os, 
+                const typename Receiver<TIME>::state_type& i) {
+            os<<"acknowledgement_number: "<<i.acknowledgement_number; 
+            return os;
+        }
 };     
 
 #endif // _BOOST_SIMULATION_PDEVS_RECEIVER_HPP_
